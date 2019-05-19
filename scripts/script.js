344ae9ad350b3218dcae05d74bfc6fb3024b62d5
sanyogita <sanyogitapandit1989@gmail.com>
2019-05-19 18:23:08 -0400

    Added keyboard support

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,32 @@
+const divMainDisp = document.querySelector("#main");
+const divSubDisp = document.querySelector("#sub");
+const bBackspace = document.querySelector("button[value=backspace]");
+const bPercentage = document.querySelector("button[value='%']");
+const bDivide = document.querySelector("button[value='/']");
+const bMultiply = document.querySelector("button[value='*']");
+const bSubtract = document.querySelector("button[value='-']");
+const bAdd = document.querySelector("button[value='+']");
+const bSign = document.querySelector("button[value='sign']");
+const bPeriod = document.querySelector("button[value='.']");
+const bEval = document.querySelector("button[value='=']");
+const bNumbers = [];
+for (let index = 0; index < 10; index++) {
+    bNumbers.push(document.querySelector("button[value='"+ index +"']"));
+}
+
 let inputString = "0";
 let operators = ["%", "/", "*", "+", "-"];
 let inputArray = [];
 
 function onButtonClick(event) {
-    let mainDispStr = document.getElementById("main").textContent;
-    let subDispStr = document.getElementById("sub").textContent;
+    let mainDispStr = divMainDisp.textContent;
+    let subDispStr = divSubDisp.textContent;
     let currentVal = event.currentTarget.value;
 
     if(isNaN(inputString) 
     && (operators.findIndex(op => op === inputString) == -1))
     {
-        document.getElementById("sub").textContent = "Ans: " + mainDispStr;
+        divSubDisp.textContent = "Ans: " + mainDispStr;
         inputString = mainDispStr = "0";
         inputArray = [];
     }
@@ -22,7 +38,7 @@ function onButtonClick(event) {
     && subDispStr.search("Ans") === -1 
     && (!isNaN(currentVal) || currentVal == ".")
     && inputArray.length == 0){
-        document.getElementById("sub").textContent = "Ans: " + mainDispStr;
+        divSubDisp.textContent = "Ans: " + mainDispStr;
         if(currentVal == "."){
             inputString = mainDispStr = "0";
         }
@@ -45,13 +61,14 @@ function onButtonClick(event) {
 
         if (currentVal === "%") {
             currentVal = inputArray[inputArray.length - 1] / 100;
-            inputArray[inputArray.length - 1] = currentVal;
-            let startIndex = mainDispStr.lastIndexOf(inputString);
+            
+            let startIndex = mainDispStr.lastIndexOf(inputArray[inputArray.length - 1]);
             if (startIndex == 0)
                 mainDispStr = "";
             else
                 mainDispStr = mainDispStr.slice(0, startIndex);
 
+            inputArray[inputArray.length - 1] = currentVal;
             inputString = "%";
         }
         else if (operators.findIndex(op => op === mainDispStr[mainDispStr.length - 1]) >= 0) {
@@ -72,7 +89,7 @@ function onButtonClick(event) {
         inputString += currentVal;
     }
 
-    document.getElementById("main").textContent = mainDispStr += currentVal;
+    divMainDisp.textContent = mainDispStr += currentVal;
 
     if (Number.MAX_VALUE == Number(mainDispStr))
         alert("");
@@ -124,21 +141,20 @@ function parseInput() {
         }
     });
 
-    let mainDisp = document.getElementById("main");
-    document.getElementById("sub").textContent = mainDisp.textContent + "=";
-    inputString = mainDisp.textContent = inputArray;
+    divSubDisp.textContent = divMainDisp.textContent + "=";
+    inputString = divMainDisp.textContent = inputArray;
     inputArray = [];
 }
 
 function clear() {
-    document.getElementById("main").textContent = "0";
-    document.getElementById("sub").textContent = "0";
+    divMainDisp.textContent = "0";
+    divSubDisp.textContent = "0";
     inputArray = [];
     inputString = "0";
 }
 
 function removeLastInput(){
-    let subDispStr = document.getElementById("sub").textContent;
+    let subDispStr = divSubDisp.textContent;
     if(subDispStr !== "0" 
     && subDispStr.search("Ans") === -1 
     && inputArray.length == 0){
@@ -146,7 +162,7 @@ function removeLastInput(){
         return;
     }
 
-    let mainDispText = document.getElementById("main").textContent;
+    let mainDispText = divMainDisp.textContent;
     if(inputArray.length > 0 || inputString.length > 0){
         if(inputString.length == 0)
         {
@@ -159,17 +175,18 @@ function removeLastInput(){
         }             
     }
 
-    if(inputArray.length == 0 && inputString.length == 0){
-        document.getElementById("main").textContent = "0";
+    if((inputArray.length == 0 && inputString.length == 0) 
+    || divMainDisp.textContent.length == 0
+    || divMainDisp.textContent.length == 1){
+        divMainDisp.textContent = "0";
     }
     else{
-        document.getElementById("main").textContent = mainDispText.slice(0, mainDispText.length -1);
+        divMainDisp.textContent = mainDispText.slice(0, mainDispText.length -1);
     }
 }
 
 function changeNumberSign()
-{    
-    let mainDisp = document.getElementById("main");
+{
     let numToBeNegated = NaN;
     if(inputString.length > 0 && !isNaN(inputString)){        
         numToBeNegated = inputString;
@@ -181,15 +198,73 @@ function changeNumberSign()
     }
 
     if(!isNaN(numToBeNegated)){
-        let indexInMainSTR = mainDisp.textContent.lastIndexOf(numToBeNegated);
+        let indexInMainSTR = divMainDisp.textContent.lastIndexOf(numToBeNegated);
         numToBeNegated *= -1; 
-        let temp = mainDisp.textContent.slice(0, indexInMainSTR);
-        mainDisp.textContent = temp + numToBeNegated.toString();
+        let temp = divMainDisp.textContent.slice(0, indexInMainSTR);
+        divMainDisp.textContent = temp + numToBeNegated.toString();
     }
 }
 
+//Keyboard actions
+window.addEventListener("keydown", function (e) {
+    if (e.key == "%") {
+        simulateClick(bPercentage);
+    }
+    if (e.key == parseFloat(e.key)) {
+        simulateClick(bNumbers[e.key])
+    }
+    if (e.keyCode == 13) {
+        simulateClick(bEval);
+    }
+    if (e.key == "=") {
+        simulateClick(bEval);
+    }
+    if (e.key == "/") {
+        simulateClick(bDivide);
+    }
+    if (e.key == "*" || e.keyCode == 88) {
+        simulateClick(bMultiply);
+    }
+    if (e.key == "+") {
+        simulateClick(bAdd);
+    }
+    if (e.key == "-") {
+        simulateClick(bSubtract);
+    }
+    if (e.key == ".") {
+        simulateClick(bPeriod);
+    }
+    if (e.keyCode == 8) {
+        simulateClick(bBackspace);
+    }
+})
+/**
+ * Simulate a click event.
+ * @public
+ * @param {Element} elem  the element to simulate a click on
+ */
+var simulateClick = function (elem) {
+    // Create our event (with options)
+    var evt = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+    // If cancelled, don't dispatch our event
+    var canceled = !elem.dispatchEvent(evt);
+};
+
 function onPageLoad() {
     Array.from(document.getElementsByTagName("button")).forEach(element => {
+
+        element.addEventListener("mousehover", event => {
+            //event.currentTarget.sty
+        });
+
+        element.addEventListener("mouseleave", event => {
+
+        });
+
         if (element.value === "=") {
             element.addEventListener("click", parseInput);
         }
